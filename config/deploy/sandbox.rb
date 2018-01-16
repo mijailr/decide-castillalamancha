247337d55123331d656c02d120ba379fe68dc4ad
Andrés Pereira de Lucena <apereira@alabs.org>
2018-01-16 18:44:18 +0100

    Removing sandbox environment and fixing deploy for staging

--- config/deploy/sandbox.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-server "decideclm.alabs.org", roles: %w(app db web worker)
-set :branch, "master"
-set :rails_env, "staging"
-set :repo_url, "https://github.com/alabs/decide-castillalamancha" # Until we have access to GitHub repository 
-
