aa0f3c787307c5773759d9010b9600982e6c4509
Mijaíl Rondon <mijail@gnu.org.ve>
2018-01-22 10:08:55 -0500

    Add menu transparencia

---
+++ config/initializers/transparencia_menu.rb
@@ -0,0 +1,6 @@
+Decidim.menu :menu do |menu|
+  menu.item I18n.t("menu.accountability"),
+            "http://transparencia.castillalamancha.es/",
+            position: 4,
+            active: false
+end
