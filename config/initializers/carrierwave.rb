ca4e8a62607d1a9b424b2b5665a9bfdae5a79de6
Betsabeth Torres <mbetsabeth@gmail.com>
2018-02-06 19:30:19 -0400

    config host for cards

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,6 +6,7 @@ CarrierWave.configure do |config|
   config.permissions = 0o666
   config.directory_permissions = 0o777
   config.storage = :file
+  config.asset_host = "https://participa.castillalamancha.es"
   config.enable_processing = !Rails.env.test?
 end
 
